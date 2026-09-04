98619f26a41d8695fc3d6be74254800dd0611d02
saylordts <saylordts@gmail.com>
2026-09-04 14:35:06 -0500

    Changed to playerLinks.csv in playerSaver, and updated table names and data calls

--- _5_Save_Results/playerSaver.py
+++ _5_Save_Results/playerSaver.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 def savePlayers(report):
-    df = pd.read_csv("_Data/players.csv")
+    df = pd.read_csv("_Data/playerLinks.csv")
     columns = df.columns.tolist()
     for player in report.players:
         last_game = max([game.date for game in player.games])
-        matching_row = df[df["URL"] == player.URL]
+        matching_row = df[df["name"] == player.name]
         if not matching_row.empty:
             print(f"Updating last game for {player.name} to {last_game.isoformat()}")
-            df.loc[df['URL'] == player.URL, "Last Played"] = last_game.isoformat()
+            df.loc[df['name'] == player.name, "Last Played"] = last_game.isoformat()
         else:
-            new_row = pd.DataFrame([[player.URL, last_game.isoformat()]], columns=columns)
+            new_row = pd.DataFrame([[player.name, last_game.isoformat()]], columns=columns)
             df = pd.concat([df, new_row], ignore_index=True)
     df.to_csv("_Data/players.csv", index=False)
