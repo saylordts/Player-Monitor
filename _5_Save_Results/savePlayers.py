0b69054c3d1a0297943ebea682a5f98088262a3a
saylordts <saylordts@gmail.com>
2026-09-10 09:02:29 -0500

    Fixed if no new games added. Need to add failure email and zero game players

--- _5_Save_Results/savePlayers.py
+++ _5_Save_Results/savePlayers.py
@@ -4,12 +4,13 @@ def savePlayers(report):
     df = pd.read_csv("_Data/playerLinks.csv")
     columns = df.columns.tolist()
     for player in report.players:
-        last_game = max([game.date for game in player.games])
-        matching_row = df[df["name"] == player.name]
-        if not matching_row.empty:
-            print(f"Updating last game for {player.name} to {last_game.isoformat()}")
-            df.loc[df['name'] == player.name, "lastPlayed"] = last_game.isoformat()
-        else:
-            new_row = pd.DataFrame([[player.name, last_game.isoformat()]], columns=columns)
-            df = pd.concat([df, new_row], ignore_index=True)
+        if player.games:
+            last_game = max([game.date for game in player.games])
+            matching_row = df[df["name"] == player.name]
+            if not matching_row.empty:
+                print(f"Updating last game for {player.name} to {last_game.isoformat()}")
+                df.loc[df['name'] == player.name, "lastPlayed"] = last_game.isoformat()
+            else:
+                new_row = pd.DataFrame([[player.name, last_game.isoformat()]], columns=columns)
+                df = pd.concat([df, new_row], ignore_index=True)
     df.to_csv("_Data/playerLinks.csv", index=False)
