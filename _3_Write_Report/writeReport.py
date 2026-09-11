e96c9e4e514882e8004d1d561ac429ef1e392f95
saylordts <saylordts@gmail.com>
2026-09-11 15:35:15 -0500

    Remove testing write

--- _3_Write_Report/writeReport.py
+++ _3_Write_Report/writeReport.py
@@ -5,7 +5,5 @@ from _3_Write_Report.writeMJML import writeMJML
 def writeReport(report: Report):
    mjml = writeMJML(report)
    html = writeHTML(mjml)
-   with open("__0_Testing/viewer.txt", "w") as file:
-      file.write(mjml)
    report.html = html
    return report
